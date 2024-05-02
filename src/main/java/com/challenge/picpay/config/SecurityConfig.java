505b70c699d3d4c1a19ae8e25cce1ccfd1da6684
pedrobvferreira <pedrobvf@hotmail.com>
2024-05-02 03:05:46 +0100

    picpay

--- src/main/java/com/challenge/picpay/config/SecurityConfig.java
+++ src/main/java/com/challenge/picpay/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.challenge.picpay.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,12 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,6 +37,7 @@ public class SecurityConfig {
                 .requestMatchers(antMatcher("/v1/**/private/**")).hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
